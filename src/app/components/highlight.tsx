b0521902c7adbdd1fc06f7fb8881b7d35f20056e
atishay-vishwakarma <atishay849@gmail.com>
2024-03-23 01:26:53 +0530

    Changes the highlights layout

--- src/app/components/highlight.tsx
+++ src/app/components/highlight.tsx
@@ -3,26 +3,36 @@ import { ArrowDownIcon, ArrowUpIcon } from "@chakra-ui/icons";
 import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
 import React from "react";
 
-const Highlight = ({ size, color}: { size: string, color:string }) => {
+const Highlight = ({
+  size,
+  color,
+  progress,
+}: {
+  size: string;
+  color: string;
+  progress: number;
+}) => {
   return (
     <>
-      <div className="flex flex-col max-h-full w-full items-center justify-center px-2 py-2 box-border">
-        <div className="flex w-full items-center justify-center gap-10">
+      <div className="flex max-h-full w-full items-center justify-between p-2 box-border">
+        <div className="flex flex-col items-start justify-start gap-1">
           <section>
-            <h3>Total Finance</h3>
+            <h3 className="font-semibold">Total Finance</h3>
             <h1>&#8377; 50,640</h1>
           </section>
-          <CircularProgress value={40} color={color} size={size}>
-            <CircularProgressLabel>40%</CircularProgressLabel>
-          </CircularProgress>
-        </div>
-        <hr className="bg-gray-200 w-full border-10" />
-        <div className="flex items-center justify-center p-2 gap-2">
-          <div className="flex flex-row items-center justify-center rounded-xl p-1 bg-green-100">
-            <ArrowUpIcon color="green.400" />
-            <h3>10%</h3>
+          <hr className="bg-gray-100 w-full border-10" />
+          <div className="flex items-center justify-start rounded-xl gap-1">
+            <div className="flex items-center bg-green-100">
+              <ArrowUpIcon color="green.400" />
+              <h3>10%</h3>
+            </div>
+            <h3 className="text-sm">Than the last week</h3>
           </div>
-          <h3>Than the last week</h3>
+        </div>
+        <div className="flex flex-col box-border">
+          <CircularProgress value={progress} color={color} size={size}>
+            <CircularProgressLabel>{progress}%</CircularProgressLabel>
+          </CircularProgress>
         </div>
       </div>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-import Image from "next/image";
-
-export default function Home() {
+'use client'
+import { FormEvent } from 'react'
+ 
+export default function Page() {
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+ 
+    const formData = new FormData(event.currentTarget)
+    const response = await fetch('/submit', {
+      method: 'POST',
+      body: formData,
+    })
+ 
+    // Handle response if necessary
+    const data = await response.json()
+    // ...
+  }
+ 
   return (
-    <></>
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" />
+      <button type="submit">Submit</button>
+    </form>
   )
 }
