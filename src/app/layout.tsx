57e02c7d8e6fe8e772e5ffdfda68d534941db58a
atishay-vishwakarma <atishay849@gmail.com>
2024-04-03 08:34:41 +0530

    API testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={`${inter.className}`}>
         {/* FULL CONTAINER DIV */}
         <div className="flex flex-col h-screen finLanding-gradient-b-1">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,44 @@
-'use client'
-import { FormEvent } from 'react'
- 
+"use client";
+import axios from "axios";
+import { FormEvent, useEffect, useState } from "react";
+import {IResponse, IAccounts } from "./models/responseModels";
+
+
+
 export default function Page() {
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
- 
-    const formData = new FormData(event.currentTarget)
-    const response = await fetch('/submit', {
-      method: 'POST',
-      body: formData,
-    })
- 
-    // Handle response if necessary
-    const data = await response.json()
-    // ...
-  }
- 
+
+  const [accounts, setAccounts] = useState<IResponse>();
+  const [users, setUsers] = useState<IAccounts[]>([]);
+
+  useEffect(() => {
+    async function fetchAccounts() {
+      try {
+        const response = await axios.get<IResponse>(
+          "https://localhost:7009/api/account/all"
+        );
+        setAccounts(response.data);
+        setUsers(response.data.result);
+      } catch (error) {
+        console.error("Error fetching forecasts:", error);
+      }
+    }
+
+    fetchAccounts();
+  }, []);
+
+  console.log(accounts);
+  console.log(users);
+
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" name="name" />
-      <button type="submit">Submit</button>
-    </form>
-  )
+    <>
+      {users.length !== 0 ? users.map((user) => (
+        <>
+          <h1>Account Id: {user.accountId}</h1>
+          <h1>Account Name: {user.name}</h1>
+          <h1>Account Created On: {user.createdOn}</h1>
+          <br></br>
+        </>
+      )) : <h1>Problem fething the API!</h1>}
+    </>
+  );
 }
